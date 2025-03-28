02d6cc63227aef6039084e13c521976f9b657125
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-03-28 08:36:01 +0000

    reorganizar codigo

--- Geometria/punto.py
+++ Geometria/punto.py
@@ -5,3 +5,23 @@ class Punto:
     
     def __str__(self):
         return f"({self.x},{self.y})"
+    
+    def determinar_cuadrante(punto):
+        if punto.x > 0 and punto.y > 0:
+            return "Primer cuadrante"
+        elif punto.x < 0 and punto.y > 0:
+            return "Segundo cuadrante"
+        elif punto.x < 0 and punto.y < 0:
+            return "Tercer cuadrante"
+        elif punto.x > 0 and punto.y < 0:
+            return "Cuarto cuadrante"
+        elif punto.x == 0 and punto.y != 0:
+            return "Eje Y"
+        elif punto.y == 0 and punto.x != 0:
+            return "Eje X"
+        else:
+            return "Origen"
+        
+    def calcular_vector(punto1, punto2):
+        return (punto2.x - punto1.x, punto2.y - punto1.y)
+    
